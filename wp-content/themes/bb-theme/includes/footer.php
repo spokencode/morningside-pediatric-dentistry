4d5e134270cf680af61478fc94ef422b5cd35814
valsimot <valsimot@gmail.com>
2016-04-04 17:13:11 -0400

    base MPD theme for migration to PROD

--- wp-content/themes/bb-theme/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="fl-page-footer">
-	<div class="fl-page-footer-container container">
-		<div class="fl-page-footer-row row">
-			<?php FLTheme::footer_col1(); ?>
-			<?php FLTheme::footer_col2(); ?>
-		</div>
-	</div>
-</div><!-- .fl-page-footer -->
